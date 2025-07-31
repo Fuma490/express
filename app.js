47dcdf4993f279c431f0dd6c208adcb4b97482a6
Fuma <haraguchifuma@gmail.com>
2025-07-31 09:18:19 +0900

    delete notes.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
-var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 
 var app = express();
@@ -16,7 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
 
 // uncomment after placing your favicon in /public
